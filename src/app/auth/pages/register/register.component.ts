8dddf26555366ec69984248d858097258480dc7d
Juan Carlos Fernández García <jucarlos@jccm.es>
2025-05-15 18:26:02 +0200

    Preparando página registro

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ValidatorService } from '../../../shared/services/validator.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +11,64 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+
+
+  
+  private fb = inject(FormBuilder);
+  
+  private validatorsService = inject(ValidatorService); 
+  private authService = inject( AuthService );
+  private router = inject(Router);
+
+  public enviandoFormulario = false;
+  public mensajeError = '';
+
+
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required]],
+    // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
+    email: ['', [ 
+      Validators.required, 
+      Validators.pattern( this.validatorsService.emailPattern )
+    ], [  ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo('password','password2')
+    ]
+  });
+
+
+  constructor(
+  
+  ) {}
+
+  isValidField( field: string ) {
+    return this.validatorsService.isValidField( this.myForm, field );
+  }
+
+  onSubmit() {
+    // Abc123
+    
+
+    this.myForm.markAllAsTouched();
+
+    this.mensajeError = '';
+
+    if ( this.enviandoFormulario === true || this.myForm.invalid ) return;
+
+    this.enviandoFormulario = true;
+
+    
+
+    
+
+
+  }
+
+
 
 }
---
+++ src/app/shared/services/validator.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl,  FormGroup, ValidationErrors } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidatorService {
+  
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+
+  public isValidField( form: FormGroup, field: string ) {
+    return form.controls[field].errors && form.controls[field].touched;
+  }
+
+
+  public isFieldOneEqualFieldTwo( field1: string, field2: string ) {
+
+    return ( formGroup: AbstractControl ): ValidationErrors | null => {
+
+      const fieldValue1 = formGroup.get(field1)?.value;
+      const fieldValue2 = formGroup.get(field2)?.value;
+
+
+      // Si devuelves un objeto es que hay errores.
+      if ( fieldValue1 !== fieldValue2 ) {
+        formGroup.get(field2)?.setErrors({ notEqual: true });
+        return { notEqual: true }
+      }
+
+      formGroup.get(field2)?.setErrors(null);
+      return null;
+    }
+
+  }
+
+
+
+}
