c6f7765295cabd8e139c6c30bcd654c30fe444a6
Juan Carlos Fernández García <jucarlos@jccm.es>
2025-05-16 10:53:35 +0200

    interface de productos

---
+++ src/app/productos/interface/product-reponse.interface.ts
@@ -0,0 +1,45 @@
+import { User } from "../../auth/interfaces/user.interface";
+
+export interface ProductsReponse {
+    count:    number;
+    pages:    number;
+    products: Product[];
+}
+
+export interface Product {
+    id:          string;
+    title:       string;
+    price:       number;
+    description: string;
+    slug:        string;
+    stock:       number;
+    sizes:       Size[];
+    gender:      Gender;
+    tags:        Tag[];
+    images:      string[];
+    user:        User;
+}
+
+export enum Gender {
+    Kid = "kid",
+    Men = "men",
+    Women = "women",
+}
+
+export enum Size {
+    L = "L",
+    M = "M",
+    S = "S",
+    Xl = "XL",
+    Xs = "XS",
+    Xxl = "XXL",
+}
+
+export enum Tag {
+    Hoodie = "hoodie",
+    Shirt = "shirt",
+    Sweatshirt = "sweatshirt",
+}
+
+
+
