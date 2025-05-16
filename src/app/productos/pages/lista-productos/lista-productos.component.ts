59800dc2bd9833ba65224fcc25fb0cef6627a613
Juan Carlos Fernández García <jucarlos@jccm.es>
2025-05-16 11:00:44 +0200

    empezamos la página de productos

--- src/app/productos/pages/lista-productos/lista-productos.component.ts
+++ src/app/productos/pages/lista-productos/lista-productos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-lista-productos',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './lista-productos.component.html',
   styleUrl: './lista-productos.component.css'
 })
-export class ListaProductosComponent {
+export class ListaProductosComponent implements OnInit {
+  
+
+  private productService = inject( ProductsService );
+  
+  
+  ngOnInit(): void {
+    
+    this.productService.getProducts().subscribe( resp => {
+
+
+      console.log( resp );
+    })
+  }
 
 }
---
+++ src/app/productos/services/products.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { ProductsReponse } from '../interface/product-reponse.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private http = inject( HttpClient );
+  private baseUrl = environment.baseUrl;
+
+  constructor() { }
+
+  public getProducts(): Observable<ProductsReponse> {
+
+    const url = `${this.baseUrl}/api/products`;
+    return this.http.get<ProductsReponse>(url);
+
+  }
+
+
+}
